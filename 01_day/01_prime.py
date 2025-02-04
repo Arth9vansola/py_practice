ca8e3f59a443d592221fc9b3749a316930a530f8
VANSOLA ARTHKUMAR MANUBHAI <146409368+Arth9vansola@users.noreply.github.com>
2025-02-04 13:24:19 +0000

    new code added

--- 01_day/01_prime.py
+++ 01_day/01_prime.py
@@ -19,3 +19,31 @@ def is_prime(n):
 test_numbers = [1, 2, 3, 4, 5, 11, 15, 17, 19, 23, 29, 31, 49, 97, 100]
 for num in test_numbers:
     print(f"{num} is Prime: {is_prime(num)}")
+
+
+# # Program to check if a number is prime or not
+
+# num = 30
+
+# # To take input from the user
+# #num = int(input("Enter a number: "))
+
+# # define a flag variable
+# flag = False
+
+# if num == 0 or num == 1:
+#     print(num, "is not a prime number")
+# elif num > 1:
+#     # check for factors
+#     for i in range(2, num):
+#         if (num % i) == 0:
+#             # if factor is found, set flag to True
+#             flag = True
+#             # break out of loop
+#             break
+
+#     # check if flag is True
+#     if flag:
+#         print(num, "is not a prime number")
+#     else:
+#         print(num, "is a prime number")
