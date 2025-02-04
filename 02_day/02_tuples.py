7b9dc08490835a78ebe3944c4a49974a901f5045
VANSOLA ARTHKUMAR MANUBHAI <146409368+Arth9vansola@users.noreply.github.com>
2025-02-04 01:04:17 +0000

    tuples

---
+++ 02_day/02_tuples.py
@@ -0,0 +1,22 @@
+# tuples are immutable --> cannot changed in main tuple
+
+a = (1,2,3,4,5,6,7,8,9,10)
+
+# print(a.count(2))
+# print(a.index(2))
+
+t = (0 ,1, 2, 3)
+
+print(t[1:])
+# print(t[::-1]) # reverse a tuple
+# print(t[::]) # print  shallow copy means it will  change the original tuple
+print(t[2:4])
+
+# packing and unpacking in tuple
+
+b =("arth","vansola",15) # packing
+(name, surname, age) = b # unpacking
+print(name)
+print(surname)
+print(age)
+
