2d8f71e67a0b497039b62a1bc747c27d05580090
VANSOLA ARTHKUMAR MANUBHAI <146409368+Arth9vansola@users.noreply.github.com>
2025-02-05 00:41:56 +0000

    format

---
+++ 03_day/02_string_formatting.py
@@ -0,0 +1,22 @@
+# format method in string
+# print("{} is my {} ever".format("arth","bestfriend"))
+# print("{} is handsome boy on the planet {}".format("arth","earth"))
+
+# print("I have a {1} and a {0}.".format("dog", "cat")) # Output: I have a cat and a dog.
+
+# print("My name is {name} and I am {age} years old.".format(name="Alice", age=25)) # Output: My name is Alice and I am 25 years old.
+
+# f - srings 
+# name = "Alice"
+# age = 25
+# print(f"My name is {name} and I am {age} years old.") # Output: My name is Alice and I am 25 years old.
+
+
+# pi = 3.14159
+# print(f"Pi rounded to 2 decimal places: {pi:.2f}")# Output: Pi rounded to 2 decimal places: 3.14
+
+# obj = {
+#     "name": "arth",
+#     "surname": "vansola",
+# }
+# print(f"my name is {obj["name"]} and my surname is {obj["surname"]}")
