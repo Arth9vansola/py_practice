05196b62782d3296436eb8c026aa42f9cf42b6ca
VANSOLA ARTHKUMAR MANUBHAI <146409368+Arth9vansola@users.noreply.github.com>
2025-02-05 00:41:43 +0000

    string

---
+++ 03_day/01_string.py
@@ -0,0 +1,51 @@
+# strip method in string
+
+# ty = " hello world   "
+# print(ty.strip())
+# print(ty.rstrip())
+
+# ty1 = "..........hello world.........."
+# print(ty1.strip("."))
+ 
+# split method in string
+
+# venom = 'We are Venom'
+# print(venom.split()) 
+
+# edie = 'We, are, Venom'
+# print(edie.split(','))
+
+# join method in string
+
+# trevor = ["apples","are", "very", "juicy"]
+# print(" ".join(trevor))
+
+# trevor1 = "apple, is , the, amazing"
+# print(" ".join(trevor1.split(",")))
+
+# trevor2 = ['h', 'e', 'l', 'l', 'o']
+# print("".join(trevor2))
+
+# replace method in string
+
+# michael = "hey there, how are you?"
+# print(michael.replace("hey", "hello"))
+
+# michael1 = "buttermilk theif why are you here?"
+# print(michael1.replace("buttermilk","butterscotch"))
+
+# find method in string
+
+# frank = "arth vansola"
+# print(frank.find("vansola"))
+
+# frank1 = " kumar"
+# print(frank1.find("kumar"))
+
+# frank2 = "arth vansola"
+# print(frank2.find("kevin"))
+# print(len(frank2))
+# cap = frank2.capitalize()
+# print(cap)
+# print(frank2)
+# print(frank2.count("a"))
