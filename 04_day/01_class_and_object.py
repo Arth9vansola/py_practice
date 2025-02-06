f229898afe29d79f7bed8ecec2f4e6c23f075304
VANSOLA ARTHKUMAR MANUBHAI <146409368+Arth9vansola@users.noreply.github.com>
2025-02-06 00:55:41 +0000

    one

---
+++ 04_day/01_class_and_object.py
@@ -0,0 +1,28 @@
+# _init_()
+# class Car:
+#     def __init__(self, brand, model):
+#         self.brand = brand  # Instance attribute
+#         self.model = model  # Instance attribute
+
+# # Creating objects
+# car1 = Car("Toyota", "Corolla")
+# car2 = Car("Honda", "Civic")
+
+# print(car1.brand, car1.model)  # Output: Toyota Corolla
+# print(car2.brand, car2.model)  # Output: Honda Civic
+
+# self
+# class Dog:
+#     def __init__(self, name, breed):
+#         self.name = name  # Assigning instance attribute
+#         self.breed = breed
+
+#     def bark(self):
+#         print(f"{self.name} says Woof!")  # Using self to refer to object’s attribute
+
+# # Creating instances
+# dog1 = Dog("Buddy", "Labrador")
+# dog2 = Dog("Charlie", "Beagle")
+
+# dog1.bark()  # Output: Buddy says Woof!
+# dog2.bark()  # Output: Charlie says Woof!
