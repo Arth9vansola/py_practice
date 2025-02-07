88390a35a5bafa87544754f220bf8fd83ad129ba
VANSOLA ARTHKUMAR MANUBHAI <146409368+Arth9vansola@users.noreply.github.com>
2025-02-07 00:57:28 +0000

    kth largest from the given list

---
+++ 05_day/06_k_largest_list.py
@@ -0,0 +1,19 @@
+# import heapq
+
+# def kth_largest(nums, k):
+#     return heapq.nlargest(k, nums)[-1]  # Get the Kth largest element
+
+# nums = [3, 2, 3, 1, 2, 4, 5, 5, 6]
+# k = 3
+# print(kth_largest(nums, k))  # Output: 5
+#  another way
+
+import heapq
+
+def kth_largest(nums, k):
+    heapq.heapify(nums)  # Convert list into a min-heap
+    return heapq.nlargest(k, nums)[-1]  # Extract Kth largest
+
+nums = [7, 10, 4, 3, 20, 15]
+k = 3
+print(kth_largest(nums, k))  # Output: 10
