1f41025f2c5c20a92a76e4f66f30670a2f7f735a
VANSOLA ARTHKUMAR MANUBHAI <146409368+Arth9vansola@users.noreply.github.com>
2025-02-07 00:56:00 +0000

    sorted

--- 04_day/07_sorted.py
+++ 04_day/07_sorted.py
@@ -18,3 +18,15 @@
 # print(sorted_words)  
 # # Output: ['kiwi', 'apple', 'cherry', 'banana']
 
+# nums = [5, 2, 9, 1, 5, 6]
+# print(sorted(nums))         # [1, 2, 5, 5, 6, 9]
+# print(sorted(nums, reverse=True))  # [9, 6, 5, 5, 2, 1]
+
+# # Sorting a list of tuples by the second value
+# data = [(1, 'B'), (3, 'A'), (2, 'C')]
+# print(sorted(data, key=lambda x: x[1]))  # [(3, 'A'), (1, 'B'), (2, 'C')]
+
+
+# The sorted() function returns a new sorted list from an iterable.
+# It uses Timsort (a hybrid sorting algorithm based on merge sort and insertion sort).
+# It has a time complexity of O(n log n).
